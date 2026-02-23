88c1ac89095793ac99fa8ae79a0bd0e942b169e3
Gábor Kazi <kazi.gabor01@gmail.com>
2026-02-23 21:27:47 +0100

    Fixed errors in main.cpp

--- main.cpp
+++ main.cpp
@@ -4,29 +4,27 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // Hiányzó aláhúzás (_) karakter
-    std::cout << '1-100 ertekek duplazasa' // String adattípust dupla " jellel kell jelölni. Lemaradt a pontosvessző az utasítás végéről.
-                                           // Kellene egy sortörés a végére.
-    for (int i = 0;) // Hiányzik a for ciklusból a korlát és a növekmény beállítása
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        b[i] = i * 2; // Hibás logika 0-tól induló ciklus miatt. Helyesen: b[i] = (i + 1) * 2; Ha i < N_ELEMENTS a ciklusban.
+        b[i] = (i + 1) * 2;
     }
-    for (int i = 0; i; i++) // Hibás korlát
+    std::cout << "Ertekek: ";
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:" // Nem szerepel kiírandó érték.
-                              // Minden iterációban kiírja a az "Ertek:"-et. Jobb volna egyszer kiírni, majd vesszővel elválasztani a számokat.
-                              // Hiányzó pontosvessző az utasítás végén.
-    }    
-    std::cout << "Atlag szamitasa: " << std::endl; // Ez a sor felesleges, mivel maga a számítás folyamata nem kerül megjelenítésre.
-    int atlag; // Az átlag nem helyes integer adattípusként, float vagy inkább double kellene.
-               // Félrevezető elnevezés: Nem az átlag maga, hanem csupán tömb elemeinek összege.
-               // Inicializálni kell 0-val, hogy ne egy memóriaszeméthez adjuk houzá az értékeket.
-    for (int i = 0; i < N_ELEMENTS, i++) // Vessző szerepel pontosvessző helyett a ciklusban.
-    {
-        atlag += b[i] // Hiányzó pontosvessző az utasítás végén.
+        std::cout << b[i];
+        if (i < N_ELEMENTS -1) std::cout << ", ";
     }
-    atlag /= N_ELEMENTS; // Hibás logika
+    std::cout << std::endl; 
+    // std::cout << "Atlag szamitasa: " << std::endl;
+    double atlag, sum = 0;
+    for (int i = 0; i < N_ELEMENTS; i++)
+    {
+        sum += b[i];
+    }
+    atlag = sum / (double)N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    // Mivel a program elején dinamikusan foglaltunk memóriát, ezt a program vége előtt fel kell szabadítani a memóraszivárgás elkerüléséhez!
+    delete b;
     return 0;
 }
